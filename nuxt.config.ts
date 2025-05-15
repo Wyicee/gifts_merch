84fbd1d3e394aa9d04e8a8355ef9db9986649471
Daniil Koltyshev <koltyshev.daniil@gmail.com>
2025-05-15 12:45:48 +0500

    feat: добавил модуль Fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image', '@pinia/nuxt'],
-  image: {
-    format: ['webp'],
-  },
-  icon: {
-    customCollections: [
-      {
-        prefix: 'gm-icon',
-        dir: './public/icons',
-      },
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image', '@pinia/nuxt', '@nuxt/fonts'],
+    fonts: {
+        families: [
+            { name: 'Gilroy', provider: 'none', },
+        ]
+    },
+    image: {
+        format: ['webp'],
+    },
+    icon: {
+        customCollections: [
+            {
+                prefix: 'gm-icon',
+                dir: './public/icons',
+            },
+        ],
+    },
+    css: [
+        'assets/styles/main.scss'
     ],
-  },
-  css: [
-    'assets/styles/main.scss'
-  ],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `
           @use "assets/styles/vars.scss" as *;
           @use "assets/styles/utils.scss" as *;
           @use "assets/styles/media.scss" as *;
           `
+                }
+            }
         }
-      }
     }
-  }
 });
